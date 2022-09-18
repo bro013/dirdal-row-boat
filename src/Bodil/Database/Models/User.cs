eeaa5162a94e49359e055b08ad0c4c156b07a625
Bjørn Rosland <bjornrosland@gmail.com>
2022-09-18 13:06:11 +0200

    modified User properties

--- src/Bodil/Database/Models/User.cs
+++ src/Bodil/Database/Models/User.cs
@@ -6,8 +6,13 @@ namespace Bodil.Database.Models
     {
         [Key]
         public Guid Id { get; set; }
+        [Required]
         public string FirstName { get; set; }
+        [Required]
         public string LastName { get; set; }
+        public long PhoneNumber { get; set; }
+        [EmailAddress]
+        public string Email { get; set; }
         public string RevervationColor { get; set; }
     }
 }
